3b66c466b3e45499de21243de3309f7c76c9cdbc
Valery <masterpiece.nevzorov@gmail.com>
2026-07-31 11:56:44 +0100

    Initial Commit

---
+++ Meta-Audio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meta-Audio
+//
+//  Created by Unknown Pleasure on 31/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meta-Audio/Meta_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meta_AudioApp.swift
+//  Meta-Audio
+//
+//  Created by Unknown Pleasure on 31/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Meta_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
